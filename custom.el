164a266ee9b83f9ee13942b9b201277a88bf5558
jonathan <jonathanfox223@gmail.com>
2019-12-16 19:40:32 -0500

    added tangle header-args to each section

--- custom.el
+++ custom.el
@@ -3,33 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(custom-safe-themes
- ;;   (quote
- ;;    ("f26c46ec3ac5737f7a236aeff395d15bc90c631908548b061d9855c2f310947f" default)))
- '(org-latex-default-packages-alist
-   (quote
-    (("AUTO" "inputenc" t)
-     ("T1" "fontenc" t)
-     ("" "fixltx2e" nil)
-     ("" "graphicx" t)
-     ("" "longtable" nil)
-     ("" "float" nil)
-     ("" "wrapfig" nil)
-     ("" "rotating" nil)
-     ("normalem" "ulem" t)
-     ("" "amsmath" t)
-     ("" "textcomp" t)
-     ("" "marvosym" t)
-     ("" "wasysym" t)
-     ("" "amssymb" t)
-     ("colorlinks=true" "hyperref" nil)
-     "\\tolerance=1000"))))
+ '(package-selected-packages
+   '(zencoding-mode yasnippet-snippets yaml-mode which-key websocket vagrant-tramp use-package-chords unkillable-scratch transpose-frame tramp-theme systemd sublimity spacegray-theme smooth-scrolling sml-modeline sicp showkey scss-mode scheme-complete request python py-autopep8 pretty-lambdada php-mode paredit ox-hugo org-projectile org-bullets ob-translate ob-sql-mode ob-http ob-cypher nginx-mode multiple-cursors multi-term markdown-preview-eww markdown-mode magit lorem-ipsum json-navigator js2-mode ibuffer-vc htmlize highlight-chars helm-swoop helm-projectile helm-flyspell helm-describe-modes helm-descbinds helm-ag groovy-mode gh-md format-sql firebelly-theme elpy dockerfile-mode cl-generic badwolf-theme anzu ag afternoon-theme ace-window ace-jump-mode 0xc)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-doc-face ((t (:foreground "pink1"))))
- '(hc-trailing-whitespace ((t (:background "#ebcb8b"))))
- '(helm-selection ((t (:inherit highlight :background "purple" :foreground "white smoke" :weight bold)))))
+ )
  ;;:background "#f2f1f0" :foreground "#4c4c4c"
--- init.el
+++ init.el
@@ -3,11 +3,16 @@
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; (if (daemonp)
-;;     (add-hook 'after-make-frame-functions
-;;               (lambda (frame)
-;;                 (select-frame frame)
-;;                 (load-theme 'spacegray t)))
-;;   (load-theme 'spacegray t))
+;; (setq backup-directory-alist `(("." . "~/.saves")))
+(setq custom-file "~/.emacs.d/custom.el")
+(setq config-file "~/.emacs.d/config.org")
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(if (daemonp)
+    (progn
+      (org-babel-load-file config-file)
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (select-frame frame)
+                  (load custom-file)))))
+;;   (org-babel-load-file (expand-file-name config-file))
+;;   (load custom-file))
